d931fc7156cf001f621446e5048948b61aebc673
Rachamim Dabbah <raydabbah@gmail.com>
2017-05-15 00:00:18 -0400

    Added const, spread and object construction.

---
+++ Array spread.js
@@ -0,0 +1,6 @@
+var numbers = [1,3,333, 090, 1];
+
+var numbers2 = [575757,23232,909090909,13];
+
+numbers.push(numbers2);
+console.log(numbers);
---
+++ const.js
@@ -0,0 +1,6 @@
+const APPLE = 'out';
+if (true) {
+  const APPLE = 'in';
+  console.log(APPLE +' 1');
+}
+console.log(APPLE +' 1');
