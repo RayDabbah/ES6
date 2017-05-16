12644672c234a624349a2abc52a79be700551f65
Rachamim Dabbah <raydabbah@gmail.com>
2017-05-16 14:33:47 -0400

    tried using template strings with tags like the video; can't figure out why before I was getting a value by using [0] on a string and now it's coming back undefined

--- template-strings-and-tags.js
+++ template-strings-and-tags.js
@@ -1,4 +1,4 @@
-function tag(strings, ...values) {
+/*function tag(strings, ...values) {
     if (values[0].split(':')[0] <= 10) {
       values[1] = 'awake';  
     } if (values[0].split(':')[0] > 10){
@@ -14,3 +14,17 @@ var message = tag`Hello! It's ${new Date().toLocaleTimeString()}, so I'm ${''} n
 console.log(message);
 var stringy = 'this is a string';
 console.log('new item: ' + stringy[2]);
+*/
+//doing it like the video
+
+function sleepy(strin, val) {
+    if (val[0] <= 18) {
+        val[1] = 'awake and full of energy!';
+    } else {
+        val[1] = 'very tired and sleepy.';
+    }
+    console.log(val);
+    return `${strin[0]}${val[0]}${strin[1]}${val[1]}${strin[2]}`;
+}
+var message2 = sleepy`Hello, it's now ${new Date(2017, 06, 21, 20).getHours()} and I am therefore ${''}`;
+console.log(message2);
