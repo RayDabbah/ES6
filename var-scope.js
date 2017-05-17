c2250cc8e4004b7cbc81d746a1c20efbcf157004
Rachamim Dabbah <raydabbah@gmail.com>
2017-05-17 11:49:14 -0400

    this is scooping without 'var' in the funtion declaration (it became global)

---
+++ var-scope.js
@@ -0,0 +1,15 @@
+//This is to show how scoping works in ES5. if you redefine it in a function without 'var' it becomes global once th function is called.
+//with 'var' it remains local
+
+var foo =  'tony';
+
+function rename() {
+     foo = 'shmerel';
+    return foo;
+}
+
+console.log(foo);
+
+rename();
+
+console.log(foo);
