98077f3a286900cc3dc5eee19c780dae8665b762
raydabbah <dabbahray@yahoo.com>
2017-05-21 23:39:47 -0400

    Now reducing into an array works

--- shoppingObject.js
+++ shoppingObject.js
@@ -6,9 +6,14 @@ var output = fs.readFileSync('shop.txt', 'utf8')
 .split('\n')
 .map(line => line.split("\t"))
 .reduce((customer, line) => {
-	customer[line[0]] = [];
+	customer[line[0]] = customer[line[0]] || [];
+	customer[line[0]].push({
+		Item: line[1], 
+		Price: line[2],
+		Quantity: line[3]
+	})
 	return customer;
 	}, {})
 
 
-console.log(output);
+console.log(JSON.stringify(output, null, 2));
